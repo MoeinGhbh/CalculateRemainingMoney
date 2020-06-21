418385221aa1d071c57cf1d171c4876317d1c9ca
= <=>
2020-06-21 14:47:49 +0200

    solve problem with another way

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 x = [1,2,5,10,20,50,100,200]
 return_Money=[]
-def calculate(pay,Money):
-    remaind= (Money*100)-(pay*100)
+def calculate(remaind):
     for i in reversed(x):
         res = remaind//i
         if res >=1:
@@ -11,5 +10,6 @@ def calculate(pay,Money):
 
 if __name__=='__main__':
     cal_cash =input('please enter your pay and money: ').split(' ')
-    print(calculate(float(cal_cash[0]),float(cal_cash[1])))
+
+    print( calculate( ( ( float(cal_cash[1])*100 )- float(cal_cash[0])*100 )))
 
