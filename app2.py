1e0432f448c1ce670d770755a24189afd58208a7
= <=>
2020-06-22 17:28:09 +0200

    edit comments

--- app2.py
+++ app2.py
@@ -1,12 +1,11 @@
 denominations = [200,100,50,20,10,5,2,1]
-# 100p is £1
+# 100c is 1-euro
 
 def returnChange(change, denominations):
 	# makes a list size of length denominations filled with 0
 	toGiveBack = [0] * len(denominations)
 
-	# goes backwards through denominations list
-	# and also keeps track of the counter, pos.
+	# keeps track of the counter, pos.
 	for pos, coin in enumerate(denominations):
 		# while we can still use coin, use it until we can't
 		while coin <= change:
@@ -16,4 +15,4 @@ def returnChange(change, denominations):
 			
 print(returnChange(13, denominations))
 # returns [0, 0, 0, 1, 1, 0, 0]
-# 1x 10p, 1x 20p
+# 1x 10c, 1x 20c
